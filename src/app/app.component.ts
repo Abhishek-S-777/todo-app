16977a36bf5b68b59ec63e5b5c4e64fe28f7f9d3
Abhishek <S1052@Alucor.local>
2022-01-29 16:35:16 +0530

    initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { timeout } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+  constructor(){
+    // setTimeout(() => {
+    //   this.title = "Changed title"
+    // },2000);
+  }
 }
+
+  
